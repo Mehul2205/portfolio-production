7ac25f061299e6fa991d177726c07f5976e93c88
Mehul Patni <mehulpatni.2205@gmail.com>
2020-05-17 22:57:43 +0530

    Init commit

---
+++ build/precache-manifest.41a863511005b74f7e5e9b83ba5c5881.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642f3fabb248639b96a3b9e18fe241cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9169b3ce441498ced58f",
+    "url": "/static/css/2.67dfcf2d.chunk.css"
+  },
+  {
+    "revision": "4d2328fd4b18aa946592",
+    "url": "/static/css/main.9d7d0ae0.chunk.css"
+  },
+  {
+    "revision": "9169b3ce441498ced58f",
+    "url": "/static/js/2.78ab7cf6.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.78ab7cf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2328fd4b18aa946592",
+    "url": "/static/js/main.e95b6080.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "0f46d20d0249edfd0d2f4fa6d01dd531",
+    "url": "/static/media/BMX_FINAL-1.0f46d20d.png"
+  },
+  {
+    "revision": "69248ec13ba66cc6653d5f3a2deb8ced",
+    "url": "/static/media/Icon.69248ec1.png"
+  },
+  {
+    "revision": "fea38022d4d92993f9c3a590edd8847f",
+    "url": "/static/media/TRY-1.fea38022.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c5d6b03fa9c385b87114a15b46ecb32b",
+    "url": "/static/media/jumbotron-bg_photo.c5d6b03f.png"
+  },
+  {
+    "revision": "96828f5c487d9867823c04ab6de8cde6",
+    "url": "/static/media/nuk-pro-back-sky.96828f5c.jpg"
+  },
+  {
+    "revision": "7e32888ce5624cd1ba2ad0e33a087d8c",
+    "url": "/static/media/nuk-pro-buildings.7e32888c.png"
+  },
+  {
+    "revision": "e2129880b950bad22b729d55a151fe1f",
+    "url": "/static/media/resume.e2129880.pdf"
+  },
+  {
+    "revision": "7f943e49c99de9c23fef7fd2e7578693",
+    "url": "/static/media/testimonial-bg-half.7f943e49.png"
+  }
+]);
