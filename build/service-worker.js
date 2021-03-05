c5c99c815b3b56fb7994ffdab63cd4d5cd81f70a
Mehul Patni <48055152+Mehul2205@users.noreply.github.com>
2021-03-05 13:22:19 +0530

    Updated portfolio - feb 2021

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41a863511005b74f7e5e9b83ba5c5881.js"
+  "/precache-manifest.78f377b6f94bd51a3a940c5f3014a40b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
